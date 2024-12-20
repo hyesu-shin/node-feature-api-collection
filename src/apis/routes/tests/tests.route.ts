401df668a4cd6460c6ad9cfead6c8004d1a719d1
hyesu <didimxmg08@gmail.com>
2024-12-20 23:43:44 +0900

    fix(ci): Modify Github Actions workflow for Swagger upload and deployment (#2)

--- src/apis/routes/tests/tests.route.ts
+++ src/apis/routes/tests/tests.route.ts
@@ -5,7 +5,7 @@ import TestsController from '@/features/tests/tests.controller';
 const router: Router = express.Router();
 
 export default ({ app } : { app: Router }) => {
-    app.use('/', router);
+    app.use('/api/v1', router);
 
     router.get('/tests', TestsController.getTest);
     // router.post('/message', TestsController.getTest);
